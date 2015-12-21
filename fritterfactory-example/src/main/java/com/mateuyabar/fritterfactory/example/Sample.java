dbb605a900727a8a29fb1cf6628a1c47ba5eb803
Mateu Yabar <mateuyabar@mateuyabar.com>
2015-12-21 14:55:41 +0100

    trying to fix compilation... comment tests

--- fritterfactory-example/src/main/java/com/mateuyabar/fritterfactory/example/Sample.java
+++ fritterfactory-example/src/main/java/com/mateuyabar/fritterfactory/example/Sample.java
@@ -1,38 +1,38 @@
-package com.mateuyabar.fritterfactory.example;
-
-import com.mateuyabar.fritterfactory.FritterFactory;
-import com.mateuyabar.fritterfactory.mold.InstanceMold;
-import com.mateuyabar.fritterfactory.mold.MapMold;
-import com.mateuyabar.fritterfactory.providers.ListItemProvider;
-import com.mateuyabar.fritterfactory.providers.ModelProvider;
-
-import java.util.List;
-
-/**
- * Created by mateuyabar on 17/12/15.
- */
-public class Sample {
-
-    public  static void main(String[] args){
-        sampleUsingClassMolds();
-
-        sampleUsingMapMolds();
-    }
-
-    private static void sampleUsingMapMolds() {
-        FritterFactory factory = new FritterFactory();
-        List<Category> categories = factory.buildList(Category.class, 3);
-        factory.addProvider(Category.class, new ListItemProvider<Category>(categories));
-        MapMold adressMold = new MapMold();
-        factory.addProvider(Person.class, new ModelProvider<>(factory, Person.class, new InstanceMold(PersonMold.class)));
-        System.out.println(factory.buildList(Person.class));
-    }
-
-    private static void sampleUsingClassMolds() {
-        FritterFactory factory = new FritterFactory();
-        List<Category> categories = factory.buildList(Category.class, 3);
-        factory.addProvider(Category.class, new ListItemProvider<Category>(categories));
-        factory.addProvider(Person.class, new ModelProvider<>(factory, Person.class, new InstanceMold(PersonMold.class)));
-        System.out.println(factory.buildList(Person.class));
-    }
-}
+//package com.mateuyabar.fritterfactory.example;
+//
+//import com.mateuyabar.fritterfactory.FritterFactory;
+//import com.mateuyabar.fritterfactory.mold.InstanceMold;
+//import com.mateuyabar.fritterfactory.mold.MapMold;
+//import com.mateuyabar.fritterfactory.providers.ListItemProvider;
+//import com.mateuyabar.fritterfactory.providers.ModelProvider;
+//
+//import java.util.List;
+//
+///**
+// * Created by mateuyabar on 17/12/15.
+// */
+//public class Sample {
+//
+//    public  static void main(String[] args){
+//        sampleUsingClassMolds();
+//
+//        sampleUsingMapMolds();
+//    }
+//
+//    private static void sampleUsingMapMolds() {
+//        FritterFactory factory = new FritterFactory();
+//        List<Category> categories = factory.buildList(Category.class, 3);
+//        factory.addProvider(Category.class, new ListItemProvider<Category>(categories));
+//        MapMold adressMold = new MapMold();
+//        factory.addProvider(Person.class, new ModelProvider<>(factory, Person.class, new InstanceMold(PersonMold.class)));
+//        System.out.println(factory.buildList(Person.class));
+//    }
+//
+//    private static void sampleUsingClassMolds() {
+//        FritterFactory factory = new FritterFactory();
+//        List<Category> categories = factory.buildList(Category.class, 3);
+//        factory.addProvider(Category.class, new ListItemProvider<Category>(categories));
+//        factory.addProvider(Person.class, new ModelProvider<>(factory, Person.class, new InstanceMold(PersonMold.class)));
+//        System.out.println(factory.buildList(Person.class));
+//    }
+//}
